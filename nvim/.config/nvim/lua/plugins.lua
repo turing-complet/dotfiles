22e3944c2519135e811b12d63e1fdbe92a0e6f1d
Jon Hagg <jhagg314@gmail.com>
2021-07-07 01:35:12 -0700

    WOW such text objects

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -14,7 +14,24 @@ require'nvim-treesitter.configs'.setup {
   highlight = {
     enable = true,
   },
+  textobjects = {
+    select = {
+      enable = true,
+
+      -- Automatically jump forward to textobj, similar to targets.vim 
+      lookahead = true,
+
+      keymaps = {
+        -- You can use the capture groups defined in textobjects.scm
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["ac"] = "@class.outer",
+        ["ic"] = "@class.inner",
+      },
+    },
+  },
 }
+
 
 
 -- ============== lsp  ===============
